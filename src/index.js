74006c585d5fcf129942ba84d1ebb9111973784a
Karol <karol.sputo@protonmail.com>
2020-03-31 14:42:51 +0200

    Initial

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+let idCount = links.length;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of Hackernews clone`,
+    feed: () => links,
+    link: (id) => links.find((link) => id === link.id) || null,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      return null;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
